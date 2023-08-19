e943012bea0236c9049d34913eb944c5d9962cdd
Jorge Carrasco <jorge.carrasco@blumar.com>
2023-08-19 16:58:31 -0400

    Update

---
+++ AnalisisMarino.py
@@ -0,0 +1,1 @@
+# Comenzaremos leyendo el archivo AnalisisMarino.csv y luego realizaremos un análisis inicial de cada variable. Posteriormente, pasaremos al análisis exploratorio detallado del conjunto de datos.
